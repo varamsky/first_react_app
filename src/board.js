9bc0a7a67645672f882ed0a7863dabe8d0ea370a
varamsky <shubhamkumar5772@gmail.com>
2020-10-01 12:26:53 +0530

    Using loops to generate squares in board.js instead of hardcoding them

--- src/board.js
+++ src/board.js
@@ -3,52 +3,79 @@ import Square from './square.js';
 
 class Board extends React.Component {
 	render() {
+
+		let boardSquares = [];
+		for (let row = 0; row < 3; row++) {
+			let boardRow = [];
+			for (let col = 0; col < 3; col++) {
+				boardRow.push(
+					<span key={(row * 3) + col}>
+						{
+							<Square
+								value={this.props.squares[(row * 3) + col]}
+								onclick={() => this.props.handleClick((row * 3) + col)}
+							/>
+						}
+					</span>
+				);
+			}
+			boardSquares.push(<div className="row" key={row}>{boardRow}</div>);
+		}
+
 		return (
 			<React.StrictMode>
-				<div>
-					<div className="row">
-						<Square
-							value={this.props.squares[0]}
-							onclick={() => this.props.handleClick(0)}
-						/>
-						<Square
-							value={this.props.squares[1]}
-							onclick={() => this.props.handleClick(1)}
-						/>
-						<Square
-							value={this.props.squares[2]}
-							onclick={() => this.props.handleClick(2)}
-						/>
+				{
+					// this uses loops to generate the squares instead of hardcoding them
+					<div>
+						{boardSquares}
 					</div>
-					<div className="row">
-						<Square
-							value={this.props.squares[3]}
-							onclick={() => this.props.handleClick(3)}
-						/>
-						<Square
-							value={this.props.squares[4]}
-							onclick={() => this.props.handleClick(4)}
-						/>
-						<Square
-							value={this.props.squares[5]}
-							onclick={() => this.props.handleClick(5)}
-						/>
-					</div>
-					<div className="row">
-						<Square
-							value={this.props.squares[6]}
-							onclick={() => this.props.handleClick(6)}
-						/>
-						<Square
-							value={this.props.squares[7]}
-							onclick={() => this.props.handleClick(7)}
-						/>
-						<Square
-							value={this.props.squares[8]}
-							onclick={() => this.props.handleClick(8)}
-						/>
-					</div>
-				</div>
+
+					// below is the squares hardcoded
+					/* <div>
+						<div className="row">
+							<Square
+								value={this.props.squares[0]}
+								onclick={() => this.props.handleClick(0)}
+							/>
+							<Square
+								value={this.props.squares[1]}
+								onclick={() => this.props.handleClick(1)}
+							/>
+							<Square
+								value={this.props.squares[2]}
+								onclick={() => this.props.handleClick(2)}
+							/>
+						</div>
+						<div className="row">
+							<Square
+								value={this.props.squares[3]}
+								onclick={() => this.props.handleClick(3)}
+							/>
+							<Square
+								value={this.props.squares[4]}
+								onclick={() => this.props.handleClick(4)}
+							/>
+							<Square
+								value={this.props.squares[5]}
+								onclick={() => this.props.handleClick(5)}
+							/>
+						</div>
+						<div className="row">
+							<Square
+								value={this.props.squares[6]}
+								onclick={() => this.props.handleClick(6)}
+							/>
+							<Square
+								value={this.props.squares[7]}
+								onclick={() => this.props.handleClick(7)}
+							/>
+							<Square
+								value={this.props.squares[8]}
+								onclick={() => this.props.handleClick(8)}
+							/>
+						</div>
+					</div> */
+				}
 			</React.StrictMode>
 		);
 	}
